7289c590b8df4c4f2d940695c4804b01902000fb
liuemi001 <64052980+liuemi001@users.noreply.github.com>
2025-01-24 15:29:23 -0800

    Update lerf.py

--- lerf/lerf.py
+++ lerf/lerf.py
@@ -30,9 +30,9 @@ class LERFModelConfig(NerfactoModelConfig):
     max_scale: float = 1.5
     """maximum scale used to compute relevancy with"""
     num_lerf_samples: int = 24
-    hashgrid_layers: Tuple[int] = (12, 12)
-    hashgrid_resolutions: Tuple[Tuple[int]] = ((16, 128), (128, 512))
-    hashgrid_sizes: Tuple[int] = (19, 19)
+    hashgrid_layers: Tuple[int, int] = (12, 12)
+    hashgrid_resolutions: Tuple[Tuple[int, int]] = ((16, 128), (128, 512))
+    hashgrid_sizes: Tuple[int, int] = (19, 19)
 
 
 class LERFModel(NerfactoModel):
